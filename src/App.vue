ab442f219bd428fd5c17410b58e15810f7ee1a5c
InkoHX <inkohx@gmail.com>
2019-10-28 09:25:14 +0900

    Add generator

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,25 @@
 <template>
-  <v-app>
+  <v-app id="app">
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-title>IconGenerator</v-toolbar-title>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-container>
+      <v-content>
+        <Generator/>
+      </v-content>
+    </v-container>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { Vue, Component } from 'vue-property-decorator'
+const Generator = () => import('@/components/Generator.vue')
 
-export default Vue.extend({
-  name: 'App',
+@Component({
   components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-});
+    Generator
+  }
+})
+export default class App extends Vue {}
 </script>
---
+++ src/components/Generator.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="generator">
+    <v-layout column align-center>
+      <h2>What is this?</h2>
+      <p>これは自分用に作ったプログラムのアイコンを作成するジェネレータです。</p><br>
+      <h3>Preview</h3><br>
+      <v-card width="512" height="512" id="preview" ref="output">
+        <p class="preview-font">{{ text }}</p>
+      </v-card><br>
+      <h3>Color</h3><br>
+      <v-row justify="space-around">
+        <v-color-picker v-model="colors.left" class="ma-2"></v-color-picker>
+        <v-color-picker v-model="colors.right" class="ma-2"></v-color-picker>
+      </v-row><br>
+      <h3>Inputs</h3><br>
+      <v-row justify="space-around">
+        <v-text-field class="ma-2" label="Icon text" type="string" v-model="text"></v-text-field>
+        <v-text-field class="ma-2" label="Font size" type="number" v-model="fontsize"></v-text-field>
+      </v-row>
+      <v-btn @click="download">download</v-btn>
+    </v-layout>
+    <component :is="style"></component>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { CreateElement } from 'vue'
+const FileSaverAsync = () => import('file-saver')
+const domtoimgAsync = () => import('dom-to-image')
+
+@Component
+export default class Generator extends Vue {
+  public colors = {
+    left: '#e66465',
+    right: '#9198e5'
+  }
+
+  public text = 'Vexera'
+
+  public fontsize = 100
+
+  get style() {
+    return {
+      render: (html: CreateElement) => {
+        return html('style', {
+          domProps: {
+            innerHTML: `
+              #preview {
+                background: linear-gradient(45deg, ${this.colors.left}, ${this.colors.right});
+              }
+
+              .preview-font {
+                font-size: ${this.fontsize}px;
+              }
+            `
+          }
+        })
+      }
+    }
+  }
+
+  public async download () {
+    const saver = await FileSaverAsync()
+    const domtoimg = await domtoimgAsync().then((value) => value.default)
+    domtoimg.toBlob(document.getElementById('preview') as Node, {
+      width: 512,
+      height: 512
+    }).then((blob: string|Blob) => {
+      saver.saveAs(blob, 'icon.png')
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+#preview {
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  display: flex;
+}
+</style>
