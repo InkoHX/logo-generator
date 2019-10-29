6520e458bc55a05e72237ab6cb7fce5420325fe1
InkoHX <inkohx@gmail.com>
2019-10-29 09:57:55 +0900

    FIxes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <v-app id="app">
     <v-app-bar app>
-      <v-toolbar-title>IconGenerator</v-toolbar-title>
+      <v-toolbar-title>LogoGenerator</v-toolbar-title>
     </v-app-bar>
 
     <v-container>
       <v-content>
-        <Generator/>
+        <Generator />
       </v-content>
     </v-container>
   </v-app>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-const Generator = () => import('@/components/Generator.vue')
+import { Vue, Component } from "vue-property-decorator";
+const Generator = () => import("@/components/Generator.vue");
 
 @Component({
   components: {
--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -2,17 +2,23 @@
   <div id="generator">
     <v-layout column align-center>
       <h2>What is this?</h2>
-      <p>これは自分用に作ったプログラムのアイコンを作成するジェネレータです。</p><br>
-      <h3>Preview</h3><br>
+      <p>これは自分用に作ったプログラムのロゴを作成するジェネレータです。</p>
+      <br />
+      <h3>Preview</h3>
+      <br />
       <v-card width="512" height="512" id="preview" ref="output">
         <p class="preview-font">{{ text }}</p>
-      </v-card><br>
-      <h3>Color</h3><br>
+      </v-card>
+      <br />
+      <h3>Color</h3>
+      <br />
       <v-row justify="space-around">
         <v-color-picker v-model="colors.left" class="ma-2"></v-color-picker>
         <v-color-picker v-model="colors.right" class="ma-2"></v-color-picker>
-      </v-row><br>
-      <h3>Inputs</h3><br>
+      </v-row>
+      <br />
+      <h3>Inputs</h3>
+      <br />
       <v-row justify="space-around">
         <v-text-field class="ma-2" label="Icon text" type="string" v-model="text"></v-text-field>
         <v-text-field class="ma-2" label="Font size" type="number" v-model="fontsize"></v-text-field>
@@ -24,26 +30,26 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-import { CreateElement } from 'vue'
-const FileSaverAsync = () => import('file-saver')
-const domtoimgAsync = () => import('dom-to-image')
+import { Vue, Component } from "vue-property-decorator";
+import { CreateElement } from "vue";
+const FileSaverAsync = () => import("file-saver");
+const domtoimgAsync = () => import("dom-to-image");
 
 @Component
 export default class Generator extends Vue {
   public colors = {
-    left: '#e66465',
-    right: '#9198e5'
-  }
+    left: "#e66465",
+    right: "#9198e5"
+  };
 
-  public text = 'Vexera'
+  public text = "Vexera";
 
-  public fontsize = 100
+  public fontsize = 100;
 
   get style() {
     return {
       render: (html: CreateElement) => {
-        return html('style', {
+        return html("style", {
           domProps: {
             innerHTML: `
               #preview {
@@ -55,20 +61,22 @@ export default class Generator extends Vue {
               }
             `
           }
-        })
+        });
       }
-    }
+    };
   }
 
-  public async download () {
-    const saver = await FileSaverAsync()
-    const domtoimg = await domtoimgAsync().then((value) => value.default)
-    domtoimg.toBlob(document.getElementById('preview') as Node, {
-      width: 512,
-      height: 512
-    }).then((blob: Blob) => {
-      saver.saveAs(blob, 'icon.png')
-    })
+  public async download() {
+    const saver = await FileSaverAsync();
+    const domtoimg = await domtoimgAsync().then(value => value.default);
+    domtoimg
+      .toBlob(document.getElementById("preview") as Node, {
+        width: 512,
+        height: 512
+      })
+      .then((blob: Blob) => {
+        saver.saveAs(blob, "icon.png");
+      });
   }
 }
 </script>
